0187d708f435c62bada2d94f607156519522dbcc
Leopold Cambier <leopoldcambier@gmail.com>
2019-10-23 14:31:25 -0700

    Checkout profiler from ptdag/master

---
+++ tools/profiler_server.py
@@ -0,0 +1,32 @@
+import numpy as np
+from bisect import bisect_left
+import argparse
+import pandas as pd
+
+from bokeh.io import output_file, output_notebook
+from bokeh.plotting import figure, show
+from bokeh.models import ColumnDataSource, Plot, LinearAxis, Grid, HoverTool, Legend, LegendItem, GlyphRenderer, Select, Slider
+from bokeh.models.widgets import TextInput
+from bokeh.models.glyphs import Quad, MultiLine
+from bokeh.models.widgets import Dropdown
+from bokeh.plotting import curdoc
+from bokeh.layouts import column
+
+import profiler
+
+parser = argparse.ArgumentParser(description='Display traces output by ttor.')
+parser.add_argument('--files', type=str, nargs='+', help='The files to combine & profile (at least one required)')
+parser.add_argument('--start', action='store', dest='start', default=0.0, type=float, help='Only plot after start')
+parser.add_argument('--end', action='store', dest='end', default=float('inf'), type=float, help='Only plot before end')
+parser.add_argument('--plot_ss', action='store_true', help='Plot the stealing success lines')
+parser.add_argument('--plot_comm', action='store_true', help='Plot the comm lines')
+parser.add_argument('--output', action='store', dest='output', type=str, help='Save profile to file', default='profile.html')
+
+args = parser.parse_args()
+
+datas = [profiler.load(f) for f in args.files]
+whos, data = profiler.combine(datas)
+text_filter, p = profiler.make_plot(whos, data, min_start=args.start, max_end=args.end, plot_ss=args.plot_ss, plot_comm=args.plot_comm)    
+
+layout = column(text_filter, p)
+curdoc().add_root(layout)
