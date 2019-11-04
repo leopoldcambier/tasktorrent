8b0a6e27079aa97c741df0044d07f16c4f850102
Yizhou Qian <43595270+qyz96@users.noreply.github.com>
2019-11-03 23:17:44 -0800

    Update tuto.cpp

--- tutorial/tuto.cpp
+++ tutorial/tuto.cpp
@@ -195,13 +195,7 @@ void tuto_1(int n_threads, int verb, int n, int nb)
             int k=kij[0];
             int i=kij[1];
             int j=kij[2];
-            int t=0;
-            if (k==0) {
-                t=2;
-            }
-            else {
-                t=3;
-            }
+            int t=2;
             if (i==j) {
                 t--;
             }
