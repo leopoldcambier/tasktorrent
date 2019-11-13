5cc412c794477d34d7cf7b2b5a712243178b9df8
Leopold Cambier <leopoldcambier@gmail.com>
2019-11-13 14:04:51 -0800

    Cmake typo + header guards + new tasktorrent.hpp

---
+++ src/tasktorrent.hpp
@@ -0,0 +1,12 @@
+#ifndef __TTOR_TASKTORRENT_HPP__
+#define __TTOR_TASKTORRENT_HPP__
+
+#include "apply_functions.hpp"
+#include "communications.cpp"
+#include "functional_extra.hpp"
+#include "runtime.hpp"
+#include "serialization.hpp"
+#include "util.hpp"
+#include "views.hpp"
+
+#endif
