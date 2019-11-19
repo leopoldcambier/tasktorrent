16ed79cf4c5433a1e0bc3ecd41ff70c0a3c6bc86
Yizhou Qian <adncat@stanford.edu>
2019-11-19 00:35:22 -0800

    Update tuto.cpp

--- tutorial/tuto.cpp
+++ tutorial/tuto.cpp
@@ -238,8 +238,12 @@ void tuto_1(int n_threads, int verb, int n, int nb)
     timer t0 = wctime();
     int err = LAPACKE_dpotrf(LAPACK_COL_MAJOR, 'L', L.rows(), L.data(), L.rows());
     timer t1 = wctime();
+    LLT<MatrixXd> Test(*A);
+    MatrixXd LR=Test.matrixL();
+    timer t2 = wctime();
     L=L.triangularView<Lower>();
     cout<<"Elapsed time: "<<elapsed(t0,t1)<<endl;
+    cout<<"Elapsed time: "<<elapsed(t1,t2)<<endl;
     cout<<"LLT Error: "<<(*A-L*L.transpose()).norm()/A->norm()<<"\n";
     //cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans, L.rows(), L.rows(), L.cols(), -1.0, L.data(), L.rows(), L.data(), L.rows(), 0.0, A->data(), L.rows());
     //cout<<"LLT Error GT: "<<(A-LR*LR.transpose()).norm()/A.norm()<<"\n";
