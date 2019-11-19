49e794e34c0da5a7d7bdb83e02dbba6a7ea1f6fa
Yizhou Qian <adncat@stanford.edu>
2019-11-19 14:01:02 -0800

    Update tuto.cpp

--- tutorial/tuto.cpp
+++ tutorial/tuto.cpp
@@ -236,18 +236,19 @@ void tuto_1(int n_threads, int verb, int n, int nb)
     // Other ranks do nothing
     // Run until completion
     tp.join();
+    MatrixXd L = A;
     timer t0 = wctime();
     int err = LAPACKE_dpotrf(LAPACK_COL_MAJOR, 'L', L.rows(), L.data(), L.rows());
     timer t1 = wctime();
     LLT<MatrixXd> Test(A);
-    L1=Test.matrixL();
+    MatrixXd L1=Test.matrixL();
     timer t2 = wctime();
     L=L.triangularView<Lower>();
     cout<<"Elapsed time for LAPACK_dpotrf: "<<elapsed(t0,t1)<<endl;
-    cout<<"Elapsed time for LAPACKE_dpotrf: "<<elapsed(t1,t2)<<endl;
+    cout<<"Elapsed time for Eigen: "<<elapsed(t1,t2)<<endl;
     cout<<"LLT Error for LAPACK: "<<(A-L*L.transpose()).norm()/A.norm()<<"\n";
     cout<<"LLT Error for Eigen : "<<(A-L1*L1.transpose()).norm()/A.norm()<<"\n";
-    cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans, L.rows(), L.rows(), L.cols(), -1.0, L.data(), L.rows(), L.data(), L.rows(), 0.0, A.data(), L.rows());
+    //cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans, L.rows(), L.rows(), L.cols(), -1.0, L.data(), L.rows(), L.data(), L.rows(), 0.0, A.data(), L.rows());
     //cout<<"LLT Error GT: "<<(A-LR*LR.transpose()).norm()/A.norm()<<"\n";
 }
 
