6715f153fe536c5f58e52710037a77cab178b380
Yizhou Qian <adncat@stanford.edu>
2019-11-20 01:12:14 -0800

    Update tuto.cpp

--- tutorial/tuto.cpp
+++ tutorial/tuto.cpp
@@ -125,7 +125,7 @@ void tuto_1(int n_threads, int verb, int n, int nb)
         L.block(i*n,k*n,n,n)=temp;
         //cout<<T(0,0)<<"\n";
 
-        L.block(i*n,k*n,n,n)=T;
+        //L.block(i*n,k*n,n,n)=T;
         //MatrixXd Temp=L.block(i*n, k*n, n, n);
         //cout<<(LR.block(i*n, k*n, n, n)-L.block(i*n, k*n, n, n)).norm()<<endl;
         //cout<<Temp(0,0)<<endl;
