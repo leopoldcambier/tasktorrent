2fffb303937555df6090ebb347795e1ce8d60603
Yizhou Qian <adncat@stanford.edu>
2019-11-20 01:19:24 -0800

    Update tuto.cpp

--- tutorial/tuto.cpp
+++ tutorial/tuto.cpp
@@ -120,11 +120,14 @@ void tuto_1(int n_threads, int verb, int n, int nb)
         int i=ki[1];
         //cout<<L.block(i*n,k*n,n,n)<<endl;
         MatrixXd temp=L.block(i*n,k*n,n,n);
-        //auto T=L.block(k*n,k*n,n,n).triangularView<Lower>().transpose().solve<OnTheRight>(L.block(i*n,k*n,n,n));
+        auto T=L.block(k*n,k*n,n,n).triangularView<Lower>().transpose().solve<OnTheRight>(L.block(i*n,k*n,n,n));
         cblas_dtrsm(CblasColMajor, CblasRight, CblasLower, CblasTrans, CblasNonUnit, n, n, 1.0, L.block(k*n,k*n,n,n).data(),n, temp.data(), n);
-        L.block(i*n,k*n,n,n)=temp;
-        //cout<<T(0,0)<<"\n";
-
+        //L.block(i*n,k*n,n,n)=temp;
+        cout<<L.block(i*n,k*n,n,n)<<"\n\n";
+        cout << "LAPACK: \n";
+        cout << temp*L.block(k*n,k*n,n,n).transpose() << "\n\n";
+        cout << "Eigen: \n";
+        cout << T*L.block(k*n,k*n,n,n).transpose()<< "\n\n";
         //L.block(i*n,k*n,n,n)=T;
         //MatrixXd Temp=L.block(i*n, k*n, n, n);
         //cout<<(LR.block(i*n, k*n, n, n)-L.block(i*n, k*n, n, n)).norm()<<endl;
