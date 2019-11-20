e4ff5aea147f105959d3bddeab46b2ed9e3888ac
Yizhou Qian <adncat@stanford.edu>
2019-11-19 17:00:58 -0800

    Update tuto.cpp

--- tutorial/tuto.cpp
+++ tutorial/tuto.cpp
@@ -73,8 +73,8 @@ void tuto_1(int n_threads, int verb, int n, int nb)
 
     // Define the task flow
     potrf.set_task([&](int k) {
-          LLT<MatrixXd> lltOfA(L.block(k*n, k*n, n, n));
-          L.block(k*n, k*n, n, n)=lltOfA.matrixL();
+          //LLT<MatrixXd> lltOfA(L.block(k*n, k*n, n, n));
+          //L.block(k*n, k*n, n, n)=lltOfA.matrixL();
           //LAPACKE_dpotrf(LAPACK_COL_MAJOR, 'L', n, L.block(k*n, k*n, n, n).data(), n);
           //MatrixXd temp=L.block(k*n, k*n, n, n);
           //cout<<temp(0,0)<<endl;
