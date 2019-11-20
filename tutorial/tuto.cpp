0733055f3107dcd164c36cc1a9199d5123b9674e
Yizhou Qian <adncat@stanford.edu>
2019-11-19 16:51:13 -0800

    Update tuto.cpp

--- tutorial/tuto.cpp
+++ tutorial/tuto.cpp
@@ -113,8 +113,7 @@ void tuto_1(int n_threads, int verb, int n, int nb)
         int i=ki[1];
         //cout<<L.block(i*n,k*n,n,n)<<endl;
         auto T=L.block(k*n,k*n,n,n).triangularView<Lower>().transpose().solve<OnTheRight>(L.block(i*n,k*n,n,n));
-        //cblas_dtrsm(CblasColMajor, CblasRight, CblasLower, CblasTrans, CblasNonUnit,
-                    n, n, 1.0, L.block(k*n,k*n,n,n).data(),n, L.block(i*n,k*n,n,n).data(), n);
+        //cblas_dtrsm(CblasColMajor, CblasRight, CblasLower, CblasTrans, CblasNonUnit, n, n, 1.0, L.block(k*n,k*n,n,n).data(),n, L.block(i*n,k*n,n,n).data(), n);
         //cout<<T(0,0)<<"\n";
         L.block(i*n,k*n,n,n)=T;
         //MatrixXd Temp=L.block(i*n, k*n, n, n);
