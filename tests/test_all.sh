1a0048fdefdc50be9d83f27d59f30cf66b724d31
Leopold Cambier <leopoldcambier@gmail.com>
2020-01-30 11:35:44 -0800

    No sanitizer tests on travis

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -30,37 +30,40 @@ then
     exit 1
 fi
 
+# Sanitizer as an option
+SAN=$2
+if [ -z "$SAN" ]
+then
+    SAN="OFF"
+fi
+
 # Shared and distributed tests
 for SHARED in OFF ON
 do
-    # With and without sanitizers
-    for SAN in OFF ADDRESS THREAD UB
-    do
-        printf "\n\nTesting SHARED = ${SHARED}, SAN = ${SAN}\n\n"
-        builddir="${dir}/build_${SHARED}_${SAN}"
-        mkdir -p $builddir
-        cd $builddir
+    printf "\n\nTesting SHARED = ${SHARED}, SAN = ${SAN}\n\n"
+    builddir="${dir}/build_${SHARED}_${SAN}"
+    mkdir -p $builddir
+    cd $builddir
 
-        printf "Building in ${builddir}...\n"
-        cmake -DTTOR_SHARED=${SHARED} -DCMAKE_BUILD_TYPE=Debug -DTTOR_SAN=${SAN} ..
-        cmake --build .
-        if [[ $SHARED -eq "OFF" ]]
-        then
-            cp $dir/tests/mpi/run_tests.sh $builddir/tests/mpi/run_tests.sh
-            cd $builddir/tests/mpi
-        else
-            cp $dir/tests/shared/run_tests.sh $builddir/tests/shared/run_tests.sh
-            cd $builddir/tests/shared
-        fi
+    printf "Building in ${builddir}...\n"
+    cmake -DTTOR_SHARED=${SHARED} -DCMAKE_BUILD_TYPE=Debug -DTTOR_SAN=${SAN} ..
+    cmake --build .
+    if [[ $SHARED -eq "OFF" ]]
+    then
+        cp $dir/tests/mpi/run_tests.sh $builddir/tests/mpi/run_tests.sh
+        cd $builddir/tests/mpi
+    else
+        cp $dir/tests/shared/run_tests.sh $builddir/tests/shared/run_tests.sh
+        cd $builddir/tests/shared
+    fi
 
-        printf "Testing ...\n"
-        ./run_tests.sh
+    printf "Testing ...\n"
+    ./run_tests.sh
 
-        if [ $? != "0" ]
-        then
-            exit 1
-        fi
-    done
+    if [ $? != "0" ]
+    then
+        exit 1
+    fi
 done
 
 printf "\n\nAll test runs are complete\n"
