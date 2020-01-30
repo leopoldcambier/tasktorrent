8a9f4fc86061a490d6842e136cca6c1c7f095b4d
Leopold Cambier <leopoldcambier@gmail.com>
2020-01-30 11:00:37 -0800

    Ups. Minor fix

--- tests/test_all.sh
+++ tests/test_all.sh
@@ -21,15 +21,14 @@ printf "TTOR's source set to ${dir}"
 # Tutorial test
 printf "\nTesting tutorial\n"
 
-# cd $dir/tutorial
+cd $dir/tutorial
+make clean
+make run
 
-# make clean
-# make run
-
-# if [ $? != "0" ]
-# then
-#     exit 1
-# fi
+if [ $? != "0" ]
+then
+    exit 1
+fi
 
 # Shared and distributed tests
 for SHARED in OFF ON
