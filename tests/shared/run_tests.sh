4acd2ee0f1084a599efebb12dda00daefba1566f
Leopold Cambier <leopoldcambier@gmail.com>
2020-04-17 15:52:50 -0700

    Add test for various tf.start locations

--- tests/shared/run_tests.sh
+++ tests/shared/run_tests.sh
@@ -7,4 +7,23 @@ then
         exit 1
 fi
 
+./tests 8 0 0 0 --gtest_filter=graph.mini --gtest_repeat=100
+
+if [ $? != "0" ]
+then
+        exit 1
+fi
+
+./tests 8 0 0 0 --gtest_filter=reduction.* --gtest_repeat=100
+
+if [ $? != "0" ]
+then
+        exit 1
+fi
+
 ./tests 32 1 32 0
+
+if [ $? != "0" ]
+then
+        exit 1
+fi
