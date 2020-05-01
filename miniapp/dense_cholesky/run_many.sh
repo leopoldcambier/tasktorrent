5ae94bd2f0d9aa6226ac277fd525bd2ec0b47bf0
Leopold Cambier <leopoldcambier@gmail.com>
2020-04-30 22:44:19 -0700

    Cholesky miniapp: scripts and Makefile conf

--- miniapp/dense_cholesky/run_many.sh
+++ miniapp/dense_cholesky/run_many.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-#SBATCH -o ttor_cholesky_%j.out
+#SBATCH -o ttor_cholesky_blocksize_%j.out
 
 hostname
 lscpu
 
-BLOCK_SIZE[0]=64
-BLOCK_SIZE[1]=128
-BLOCK_SIZE[2]=256
-BLOCK_SIZE[3]=512
-BLOCK_SIZE[4]=1024
+BLOCK_SIZE[0]=32
+BLOCK_SIZE[1]=64
+BLOCK_SIZE[2]=128
+BLOCK_SIZE[3]=256
+BLOCK_SIZE[4]=512
+BLOCK_SIZE[5]=1024
+BLOCK_SIZE[6]=2048
     
 mpirun -n ${SLURM_NTASKS} hostname
 
-for i in {0..4}
+for i in 6
 do
 
     echo ${MATRIX_SIZE}
@@ -21,7 +23,7 @@ do
     echo ${NUM_BLOCKS}
 
     # ./cholesky block_size num_blocks n_threads verb nprows npcols kind log depslog test accumulate
-    OMP_NUM_THREADS=1 mpirun -n ${SLURM_NTASKS} ./cholesky ${BLOCK_SIZE[i]} ${NUM_BLOCKS} ${N_THREADS} 0 ${NPROWS} ${NPCOLS} ${KIND} 0 0 0 0
+    mpirun -n ${SLURM_NTASKS} ./cholesky ${BLOCK_SIZE[i]} ${NUM_BLOCKS} ${N_THREADS} 0 ${NPROWS} ${NPCOLS} ${KIND} 0 0 0 0
     echo "=================================="
 
 done
