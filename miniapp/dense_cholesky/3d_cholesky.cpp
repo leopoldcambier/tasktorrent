8c8dfc1a6d2bb210d189229fdc1c87f5fca09ea3
qyz96 <qyz96@github.com>
2020-08-08 23:34:06 -0700

    3d_cholesky distributed correctness pased

--- miniapp/dense_cholesky/3d_cholesky.cpp
+++ miniapp/dense_cholesky/3d_cholesky.cpp
@@ -26,7 +26,7 @@ using namespace ttor;
 typedef array<int, 2> int2;
 typedef array<int, 3> int3;
 
-void cholesky3d(int n_threads, int verb, int n, int nb, int n_col, int n_row, int priority, int test, int LOG, int tm, int debug)
+void cholesky3d(int n_threads, int verb, int n, int nb, int n_col, int n_row, int priority, int test, int log, int debug)
 {
     const int rank = comm_rank();
     const int n_ranks = comm_size();
@@ -61,16 +61,10 @@ void cholesky3d(int n_threads, int verb, int n, int nb, int n_col, int n_row, in
     struct acc_data {
         vector<std::unique_ptr<MatrixXd>> to_accumulate; // to_accumulate[k] holds matrix result of gemm(k,i,j)
     };
-
-
-
-
-
     std::vector<acc_data> gemm_results(nb*nb);
-
     auto val = [&](int i, int j) { return 1/(double)((i-j)*(i-j)+1); };
     auto rank3d21 = [&](int i, int j, int k) { return ((j % q) * q + k % q) + (i % q) * q * q;};
-    auto rank2d21 = [&](int i, int j) { return (j % n_col) * n_row + i % n_row;};
+    auto rank2d21 = [&](int i, int j) { return (j % n_col) * n_row + (i % n_row);};
     auto rank1d21 = [&](int k) { return k % n_ranks; };
     vector<unique_ptr<MatrixXd>> blocs(nb*nb);
 
@@ -87,6 +81,14 @@ void cholesky3d(int n_threads, int verb, int n, int nb, int n_col, int n_row, in
         return (ii + jj * num_blocksit) % n_threads;
     };
 
+    {
+        Eigen::MatrixXd A = Eigen::MatrixXd::Identity(256,256);
+        Eigen::MatrixXd B = Eigen::MatrixXd::Identity(256,256);
+        Eigen::MatrixXd C = Eigen::MatrixXd::Identity(256,256);
+        for(int i = 0; i < 10; i++) {
+            cblas_dgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, 256, 256, 256, 1.0, A.data(), 256, B.data(), 256, 1.0, C.data(), 256);
+        }
+    }
 
     for (int ii=0; ii<nb; ii++) {
         for (int jj=0; jj<nb; jj++) {
@@ -107,17 +109,9 @@ void cholesky3d(int n_threads, int verb, int n, int nb, int n_col, int n_row, in
             
             else {
                 blocs[ii+jj*nb]=make_unique<MatrixXd>(n, n);
-                //blocs[ii+jj*nb]=make_unique<MatrixXd>(n, n);
-                //*blocs[ii+jj*nb]=MatrixXd::Zero(n, n);
             }
         }
     }
-
-
-
-    // Map tasks to rank
-    
-
     // Initialize the communicator structure
     Communicator comm(MPI_COMM_WORLD, verb);
 
@@ -130,19 +124,20 @@ void cholesky3d(int n_threads, int verb, int n, int nb, int n_col, int n_row, in
 
     
     DepsLogger dlog(1000000);
-    Logger log(1000000);
+    Logger ttorlog(1000000);
 
-    if (LOG)  {
-        tp.set_logger(&log);
-        comm.set_logger(&log);
+    if (log)  {
+        tp.set_logger(&ttorlog);
+        comm.set_logger(&ttorlog);
     }
     
 
     // Create active message
     auto am_trsm = comm.make_large_active_msg( 
             [&](int& j) {
-                int offset = (j / n_row + (j % n_row) / (rank_2d[0] + 1)) * n_row + rank_2d[0] + 1;
+                int offset = ((j + 1) / n_row + (((j + 1) % n_row) > rank_2d[0])) * n_row + rank_2d[0];
                 for(int i = offset; i < nb; i = i + n_row) {
+                    if (debug) printf("Fulfilling trsm (%d, %d) on rank (%d, %d)\n", i, j, rank_2d[0], rank_2d[1]);
                     trsm.fulfill_promise({j,i});
                 }
             },
@@ -157,13 +152,19 @@ void cholesky3d(int n_threads, int verb, int n, int nb, int n_col, int n_row, in
     auto am_gemm = comm.make_large_active_msg(
         [&](int& i, int& k) {
             assert(k % q == rank_3d[2]);
-            int offset_c = (k / n_col + (k % n_col) / (rank_3d[1] + 1)) * n_col + rank_3d[1] + 1; 
-            for(int j = offset_c; j < i; j = j + n_col ) {
-                gemm.fulfill_promise({k,i,j});
+            int offset_c = ((k + 1) / q + (((k + 1) % q) > rank_3d[1])) * q + rank_3d[1]; 
+            if (i % q == rank_3d[0]) {
+            	for(int j = offset_c; j < i; j = j + q) {
+                        if (debug) printf("TRSM (%d, %d) Fulfilling gemm (%d, %d, %d) on rank (%d, %d, %d)\n", i, k, k, i, j, rank_3d[2], rank_3d[0], rank_3d[1]);
+                	gemm.fulfill_promise({k,i,j});
+            	}
             }
-	    int offset_r = (i / n_row + (i % n_row) / (rank_3d[0] + 1)) * n_row + rank_3d[0];
-            for(int j = offset_r; j < nb; j = j + n_row) {
-                gemm.fulfill_promise({k,j,i});
+	    int offset_r = (i / q + ((i % q) > rank_3d[0])) * q + rank_3d[0];
+            if (i % q == rank_3d[1]) {
+            	for(int j = offset_r; j < nb; j = j + q) {
+                        if (debug) printf("TRSM (%d, %d) Fulfilling gemm (%d, %d, %d) on rank (%d, %d, %d)\n", i,k, k, j, i, rank_3d[2], rank_3d[0], rank_3d[1]);      
+                	gemm.fulfill_promise({k,j,i});
+            	}
             }
         },
         [&](int& i, int& k) {
@@ -183,20 +184,20 @@ void cholesky3d(int n_threads, int verb, int n, int nb, int n_col, int n_row, in
           if (debug) printf("Running POTRF %d on rank %d, %d, %d\n", j, rank_3d[0], rank_3d[1], rank_3d[2]);
       })
         .set_fulfill([&](int j) { 
-            for (int r = 0; r < n_ranks; r++) // Looping through all outgoing dependency edges
-            {
+            for (int p = 0; p < n_row; p++) // Looping through all outgoing dependency edges
+            {   
+                int r = rank2d21(p,j);
                 if (rank == r) {
-                   int offset = (j / n_row + (j % n_row) / (rank_2d[0] + 1)) * n_row + rank_2d[0] + 1;
+                   int offset = ((j + 1) / n_row + ((j + 1) % n_row) / (rank_2d[0] + 1)) * n_row + rank_2d[0];
                    for(int i = offset; i < nb; i = i + n_row) {
-                      trsm.fulfill_promise({j,i});
+                       trsm.fulfill_promise({j,i});
                    }
                 }
                 else {
-                    auto Ljjv = view<double>(blocs[j+j*nb]->data(), n*n);
-                    am_trsm->send_large(r, Ljjv, j);
-
+                   //printf("%d \n", r);
+                   auto Ljjv = view<double>(blocs[j+j*nb]->data(), n*n);
+                   am_trsm->send_large(r, Ljjv, j);
                 }
-
             }
         })
         .set_indegree([&](int j) {
@@ -212,10 +213,7 @@ void cholesky3d(int n_threads, int verb, int n, int nb, int n_col, int n_row, in
             }
         })
         .set_mapping([&](int j) {
-            if (tm) {
-                return block_2_thread(j,j);
-            }
-            return (j % n_threads);
+            return block_2_thread(j,j);
         })
         .set_binding([&](int j) {
             return false;
@@ -254,32 +252,35 @@ void cholesky3d(int n_threads, int verb, int n, int nb, int n_col, int n_row, in
         cblas_dtrsm(CblasColMajor, CblasRight, CblasLower, CblasTrans, CblasNonUnit, n, n, 1.0, blocs[k+k*nb]->data(),n, blocs[i+k*nb]->data(), n);
         timer t2 = wctime();
         trsm_us_t += 1e6 * elapsed(t1, t2);
-        //trsm_us_t += 1;
-        if (debug) printf("Running TRSM (%d, %d) on rank %d, %d, %d\n", i, k, rank_3d[0], rank_3d[1], rank_3d[2]);
-        //cout<<(*blocs[i+k*nb])<<"\n";
-
+        if (debug) printf("Running trsm (%d, %d) on rank %d, %d\n", i, k, rank_2d[0], rank_2d[1]);
       })
         .set_fulfill([&](int2 ki) {
             int k=ki[0];
             int i=ki[1]; 
-            for (int r = 0; r < n_ranks; r++)   // Looping through all outgoing dependency edges
+            for (int ri = 0; ri < q; ri++)   // Looping through all outgoing dependency edges
             {
+                for (int rj = 0; rj < q; rj++) {
+                int r = rank3d21(ri, rj, k);
                 if (r == rank) {
-                  //if ((k % q) != rank_3d[2]) continue;
-                  int offset_c = (k / n_col + (k % n_col) / (rank_3d[1] + 1)) * n_col + rank_3d[1] + 1;
-                  for(int j = offset_c; j < i; j = j + n_col ) {
-                    gemm.fulfill_promise({k,i,j});
+                      int offset_c = ((k + 1) / q + (((k + 1) % q) > rank_3d[1])) * q + rank_3d[1];
+                      if (i % q == rank_3d[0]) {
+                      	for(int j = offset_c; j < i; j = j + q) {
+                        	gemm.fulfill_promise({k,i,j});
+                      	}
+                      }
+                      int offset_r = (i / q + ((i % q) > rank_3d[0])) * q + rank_3d[0];
+                      if (i % q == rank_3d[1]) {
+                      	for(int j = offset_r; j < nb; j = j + q) {
+                        	gemm.fulfill_promise({k,j,i});
+                      	} 
+                      }
                   }
-                  int offset_r = (i / n_row + (i % n_row) / (rank_3d[0] + 1)) * n_row + rank_3d[0];
-                  for(int j = offset_r; j < nb; j = j + n_row) {
-                    gemm.fulfill_promise({k,j,i});
-                  }
-                }
                 else {
                     //cout<<"Sending data from "<<rank<<" to "<<r<<"\n";
-                    auto Lijv = view<double>(blocs[i+k*nb]->data(), n*n);
-                    am_gemm->send_large(r, Lijv, i, k);
-                }
+                      auto Lijv = view<double>(blocs[i+k*nb]->data(), n*n);
+                      am_gemm->send_large(r, Lijv, i, k);
+                } 
+               }
             }            
         })
         .set_indegree([&](int2 ki) {
@@ -298,10 +299,7 @@ void cholesky3d(int n_threads, int verb, int n, int nb, int n_col, int n_row, in
         .set_mapping([&](int2 ki) {
             int k=ki[0];
             int i=ki[1];
-            if (tm) {
-                return block_2_thread(i,k);
-            }
-            return ((k*n+i) % n_threads);
+            return block_2_thread(i,k);
         })
         .set_binding([&](int2 ki) {
             int k=ki[0];
@@ -363,7 +361,7 @@ void cholesky3d(int n_threads, int verb, int n, int nb, int n_col, int n_row, in
                 cblas_dgemm(CblasColMajor, CblasNoTrans, CblasTrans, n, n, n, -1.0,blocs[i+k*nb]->data(), n, blocs[j+k*nb]->data(), n, 1.0, blocs[i+j*nb]->data(), n);
             }
             timer t2 = wctime();
-            if (debug) printf("Running GEMM (%d, %d, %d) on rank %d, %d, %d\n", i, j, k, rank_3d[0], rank_3d[1], rank_3d[2]);
+            if (debug) printf("Running GEMM (%d, %d, %d) on rank %d, %d, %d\n", k, i, j, rank_3d[2], rank_3d[0], rank_3d[1]);
             //gemm_us_t += 1e6 * elapsed(t1,t2);
             gemm_us_t += 1;
             
@@ -380,15 +378,14 @@ void cholesky3d(int n_threads, int verb, int n, int nb, int n_col, int n_row, in
             else {
                 int dest = rank2d21(i, j);
                 if (dest == rank) {
-                    if (debug) printf("Gemm (%d, %d, %d) fulfilling ACCUMU (%d, %d, %d) on rank %d, %d, %d\n", k, i, j, rank_3d[2], i, j, rank_3d[0], rank_3d[1], rank_3d[2]);
-
+                    if (debug) printf("gemm (%d, %d, %d) fulfilling accumu (%d, %d, %d) on rank %d, %d, %d\n", k, i, j, rank_3d[2], i, j, rank_3d[2], rank_3d[0], rank_3d[1]);
                     accu.fulfill_promise({rank_3d[2], i, j});
                 }
 
                 else {
                     int kk = rank_3d[2];
                     auto Lij = view<double>(blocs[i+j*nb]->data(), n*n);
-                    if (debug) printf("Gemm (%d, %d, %d) Sending ACCUMU (%d, %d, %d) to rank %d, %d\n", k, i, j, rank_3d[2], i, j, dest % n_row, dest / n_row);
+                    if (debug) printf("gemm (%d, %d, %d) Sending accumu (%d, %d, %d) to rank %d, %d\n", k, i, j, rank_3d[2], i, j, dest % n_row, dest / n_row);
                     am_accu->send_large(dest, Lij, i, j, kk);
                 }
             }
@@ -412,12 +409,7 @@ void cholesky3d(int n_threads, int verb, int n, int nb, int n_col, int n_row, in
             int k=kij[0];
             int i=kij[1];
             int j=kij[2];
-
-            if (tm) {
-                return block_2_thread(i,j);
-            }
-            return ((k*n*n+i+j*n)  % n_threads);
-            //return block_2_thread(i,j);
+            return block_2_thread(i,j);
         })
         .set_binding([&](int3 kij) {
             return false;
@@ -451,7 +443,7 @@ void cholesky3d(int n_threads, int verb, int n, int nb, int n_col, int n_row, in
             int k=kij[0];
             int i=kij[1];
             int j=kij[2];
-            return "GEMM" + to_string(k) + "_" + to_string(i)+"_"+to_string(j)+"_"+to_string(comm_rank());
+            return "gemm" + to_string(k) + "_" + to_string(i)+"_"+to_string(j)+"_"+to_string(comm_rank());
         });
 
     accu.set_task([&](int3 kij) {
@@ -461,22 +453,13 @@ void cholesky3d(int n_threads, int verb, int n, int nb, int n_col, int n_row, in
             int j=kij[2]; // Col
             assert(rank2d21(i,j) == rank);
             assert(j <= i);
-            if (debug) printf("Running ACCU (%d, %d, %d) on rank %d, %d\n", k, i, j, rank % n_row, rank / n_row);
-            //assert(k < j);
-            //std::unique_ptr<Eigen::MatrixXd> Atmp;
+            if (debug) printf("Running accumu (%d, %d, %d) on rank %d, %d\n", k, i, j, rank % n_row, rank / n_row);
             {
                 timer t_ = wctime();
                 *blocs[i+j*nb] += (*gemm_results[i+j*nb].to_accumulate[k]);
                 timer t__ = wctime();
                 accu_us_t += 1e6 * elapsed(t_, t__);
             }
-            //cout<<(*blocs[i+j*nb])<<"\n";
-            //cout<<(*Atmp)<<"\n";
-            //timer t_ = wctime();
-            //*blocs[i+j*nb] += (*Atmp);
-            //timer t__ = wctime();
-            //accu_us_t += 1e6 * elapsed(t_, t__);
-            //printf("Running ACCU (%d, %d, %d) on rank %d, %d\n", k, i, j, rank % n_row, rank / n_row);
         })
         .set_fulfill([&](int3 kij) {
             //assert(rank_3d21(kij[1],kij[2],kij[2]) == rank);
@@ -500,11 +483,7 @@ void cholesky3d(int n_threads, int verb, int n, int nb, int n_col, int n_row, in
             int k=kij[0];
             int i=kij[1];
             int j=kij[2];
-            if (tm) {
-                return block_2_thread(i,j);
-            }
-            //return block_2_thread(i,j);
-            return ((i+j*n)  % n_threads);
+            return block_2_thread(i,j);
             //return ((k*n*n+i+j*n)  % n_threads);// IMPORTANT. Every (i,j) should map to a given fixed thread
         })
         .set_priority([&](int3 kij) {
@@ -540,7 +519,7 @@ void cholesky3d(int n_threads, int verb, int n, int nb, int n_col, int n_row, in
             int k=kij[0];
             int i=kij[1];
             int j=kij[2];
-            return "ACCUMU" + to_string(k) + "_" + to_string(i)+"_"+to_string(j)+"_"+to_string(comm_rank());
+            return "accumu" + to_string(k) + "_" + to_string(i)+"_"+to_string(j)+"_"+to_string(comm_rank());
         });
 
 
@@ -560,7 +539,7 @@ void cholesky3d(int n_threads, int verb, int n, int nb, int n_col, int n_row, in
     MPI_Status status;
     
     if (rank==0) {
-        cout<<"3D, Number of ranks "<<n_ranks<<", n "<<n<<", nb "<<nb<<", n_threads "<<n_threads<<", tm "<<tm<<", Priority "<<priority<<", Elapsed time: "<<elapsed(t0,t1)<<endl;
+        cout<<"3D, Number of ranks "<<n_ranks<<", n "<<n<<", nb "<<nb<<", n_threads "<<n_threads<<", Priority "<<priority<<", Elapsed time: "<<elapsed(t0,t1)<<endl;
     }
 
     if (test)   {
@@ -611,11 +590,11 @@ void cholesky3d(int n_threads, int verb, int n, int nb, int n_col, int n_row, in
         }
     }
 
-    if (LOG)  {
+    if (log)  {
         std::ofstream logfile;
         string filename = "ttor_3Dcholesky_Priority_"+to_string(n)+"_"+to_string(nb)+"_"+ to_string(n_threads)+"_"+ to_string(n_ranks)+"_"+ to_string(priority)+".log."+to_string(rank);
         logfile.open(filename);
-        logfile << log;
+        logfile << ttorlog;
         logfile.close();
     }
 
@@ -641,7 +620,6 @@ int main(int argc, char **argv)
     int priority=0;
     int test=0;
     int log=0;
-    int tm=0;
     int debug=0;
 
     if (argc >= 2)
@@ -678,13 +656,10 @@ int main(int argc, char **argv)
     }
 
     if (argc >= 11) {
-        tm = atoi(argv[11]);
-    }
-    if (argc >= 12) {
-        debug = atoi(argv[12]);
+        debug = atoi(argv[10]);
     }
 
-    cholesky3d(n_threads, verb, n, nb, n_col, n_row, priority, test, log, tm, debug);  
+    cholesky3d(n_threads, verb, n, nb, n_col, n_row, priority, test, log, debug);  
 
     MPI_Finalize();
 }
