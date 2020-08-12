423ae5e0aa9332e620a65fe5fbbd094dace78d3f
Yizhou Qian <43595270+qyz96@users.noreply.github.com>
2020-08-12 13:10:51 -0700

    Cholesky: adding 3D cholesky
    Co-authored-by: qyz96 <qyz96@github.com>

--- miniapp/dense_cholesky/run_cholesky.sh
+++ miniapp/dense_cholesky/run_cholesky.sh
@@ -9,5 +9,8 @@ mpirun -n ${SLURM_NTASKS} hostname
 echo ${KIND}
 echo ${RANDOM_SIZES}
 
+# ./cholesky block_size num_blocks n_threads verb nprows npcols kind test log debug
+OMP_NUM_THREADS=1 mpirun -n ${SLURM_NTASKS} ./3d_cholesky ${BLOCK_SIZE} ${NUM_BLOCKS} ${N_THREADS} 0 ${NPROWS} ${NPCOLS} ${KIND}
+
 # ./cholesky block_size num_blocks n_threads verb nprows npcols kind log depslog test accumulate random_sizes
-OMP_NUM_THREADS=1 mpirun -n ${SLURM_NTASKS} ./cholesky ${BLOCK_SIZE} ${NUM_BLOCKS} ${N_THREADS} 0 ${NPROWS} ${NPCOLS} ${KIND} 0 0 0 0 ${RANDOM_SIZES}
+OMP_NUM_THREADS=1 mpirun -n ${SLURM_NTASKS} ./2d_cholesky ${BLOCK_SIZE} ${NUM_BLOCKS} ${N_THREADS} 0 ${NPROWS} ${NPCOLS} ${KIND} 0 0 0 0 ${RANDOM_SIZES}
